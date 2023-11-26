86162a82feafd734a085e7027f164661227b7796
Parva <parvadesai404@gmail.com>
2023-11-26 23:46:47 +0530

    fix: Scripts' renamed + Refactoring

---
+++ Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    [HideInInspector] public float speed = 0.01f;
+    private ObjectSpawner objSpawner;
+    
+    void Start()
+    {
+        if (objSpawner == null)
+            objSpawner = FindObjectOfType<ObjectSpawner>();
+    }
+    
+    void Update()
+    {
+        //to move the enemy car on road by decreasing its y component gradually
+        transform.position = new Vector3(transform.position.x, transform.position.y - (speed * Time.deltaTime), transform.position.z); 
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //  If the generated obj collide with an another then destroy the one which was generated last
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.transform.position.y > transform.position.y)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private ObjectMovement[] objPool = new ObjectMovement[9];
+    [SerializeField] private float delayTimer = 5f;
+    private float timer = 0f;
+    
+    void Start()
+    {
+        // InvokeRepeating(nameof(GenerateObj), 0f, 5f);
+        timer = delayTimer;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            GenerateObj();
+
+            timer = delayTimer;
+        }
+    }
+
+    private void GenerateObj()
+    {
+        int i = Random.Range(0, objPool.Length);
+        Vector2 pos = new Vector2(Random.Range(-2.2f, 2.2f), transform.position.y);   //set pos of generation of the car 
+        Instantiate(objPool[i].gameObject, pos, Quaternion.identity, transform);    //instantiate the car on parent pos
+    }
+
+    public void SetSpeed(float s)
+    {
+        foreach (var obj in objPool)
+        {
+            obj.speed = s;
+        }
+    }
+}
