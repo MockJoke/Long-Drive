88d29e1cefe8a442693f6e4aa3cc6fed948164a9
Parva <parvadesai404@gmail.com>
2023-11-27 23:53:06 +0530

    fix: Updated the settings system
    Moved the settings system being a standalone scene to a prefab in different scenes. Renamed the script.

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -1,45 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Settings : MonoBehaviour
+public class SettingsManager : MonoBehaviour
 {
-    private int controls;
+    private int controls = 1;
+    public Action OnClose;
 
     public void ButtonsBtn()
     {
         controls = 1;
         PlayerPrefs.SetInt("controls", controls);
-        SceneManager.LoadScene("Play");
     }
 
     public void KeysBtn()
     {
         controls = 2;
         PlayerPrefs.SetInt("controls", controls);
-        SceneManager.LoadScene("Play");
     }
 
     public void TouchBtn()
     {
         controls = 3; 
         PlayerPrefs.SetInt("controls", controls);
-        SceneManager.LoadScene("Play");
     }
 
     public void SensorBtn()
     {
         controls = 4;
         PlayerPrefs.SetInt("controls", controls);
-        SceneManager.LoadScene("Play");
     }
 
     public void PlayBtn()
     {
-        SceneManager.LoadScene("Play"); 
+        SceneManager.LoadScene("Play");
     }
 
     public void HomeBtn()
     {
-        SceneManager.LoadScene("Home"); 
+        SceneManager.LoadScene("Home");
+    }
+
+    public void CloseMenu()
+    {
+        gameObject.SetActive(false);
+        OnClose?.Invoke();
+    }
+
+    public void OpenMenu()
+    {
+        gameObject.SetActive(true);
+    }
+    
+    public void ResetCloseAction()
+    {
+        OnClose = null;
+    }
+    
+    public void MusicBtn()
+    {
+        AudioManager.instance.ToggleMusic();
     }
 }
