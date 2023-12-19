881cf1f9d7a8ff790c74e7f0fbd5fb1839af9cb4
Parva <parvadesai404@gmail.com>
2023-12-19 14:36:47 +0530

    fix: Minor refactoring

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -4,7 +4,6 @@ public class Road : MonoBehaviour
 {
     [SerializeField] private Renderer road;
     [SerializeField] private float speed = 0.1f;
-    private Vector2 offset;
     // private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
     void Start()
@@ -15,8 +14,7 @@ public class Road : MonoBehaviour
 
     void Update()
     {
-        offset = new Vector2(0, Time.time * speed);
-        road.material.mainTextureOffset = offset;
+        road.material.mainTextureOffset = new Vector2(0, Time.time * speed);
         
         // textureOffset += Time.deltaTime;
         // Vector2 offset = new Vector2(0, textureOffset);     //set offset of road 
