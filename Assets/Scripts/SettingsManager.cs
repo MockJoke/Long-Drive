e2916e301c595d04efcaf7ef6216c00111ac060a
Parva <parvadesai404@gmail.com>
2024-01-24 09:10:38 +0530

    fix: Minor refactoring

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -22,28 +22,28 @@ public class SettingsManager : MonoBehaviour
     {
         controls = 1;
         PlayerPrefs.SetInt("controls", controls);
-        Toast.Show ("Controls have been set to <b><color=black>Buttons</color></b>", 3f, ToastColor.Green);
+        Toast.Show("Controls have been set to <b><color=black>Buttons</color></b>", 3f, ToastColor.Green);
     }
 
     public void KeysBtn()
     {
         controls = 2;
         PlayerPrefs.SetInt("controls", controls);
-        Toast.Show ("Controls have been set to <b><color=black>Keys</color></b>", 3f, ToastColor.Green);
+        Toast.Show("Controls have been set to <b><color=black>Keys</color></b>", 3f, ToastColor.Green);
     }
 
     public void TouchBtn()
     {
         controls = 3; 
         PlayerPrefs.SetInt("controls", controls);
-        Toast.Show ("Controls have been set to <b><color=black>Touch</color></b>", 3f, ToastColor.Green);
+        Toast.Show("Controls have been set to <b><color=black>Touch</color></b>", 3f, ToastColor.Green);
     }
 
     public void SensorBtn()
     {
         controls = 4;
         PlayerPrefs.SetInt("controls", controls);
-        Toast.Show ("Controls have been set to <b><color=black>Sensors</color></b>", 3f, ToastColor.Green);
+        Toast.Show("Controls have been set to <b><color=black>Sensors</color></b>", 3f, ToastColor.Green);
     }
 
     public void CloseMenu()
