ec1ec860dd66d7ee2fc7c3059302f945bfe97e37
Parva <parvadesai404@gmail.com>
2024-01-31 13:56:47 +0530

    fix: Updated the road speed logic
    Setting the texture offset using Time.deltaTime instead of Time.time

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -6,7 +6,8 @@ public class Road : MonoBehaviour
     [SerializeField] private Material[] roadMaterials;
     [SerializeField] private float speed = 0.1f;
     // private static readonly int MainTex = Shader.PropertyToID("_MainTex");
-
+    // private float textureOffset = 0f;
+    
     void Awake()
     {
         if (road == null)
@@ -21,11 +22,13 @@ public class Road : MonoBehaviour
 
     void Update()
     {
-        road.material.mainTextureOffset = new Vector2(0, Time.time * speed);
+        // road.material.mainTextureOffset = new Vector2(0, Time.time * speed);
+
+        road.material.mainTextureOffset += new Vector2(0, Time.deltaTime * speed);
         
-        // textureOffset += Time.deltaTime;
-        // Vector2 offset = new Vector2(0, textureOffset);     //set offset of road 
-        // Roads.material.SetTextureOffset(MainTex, offset);    //move road by changing the texture
+        // textureOffset += Time.deltaTime * speed;
+        // Vector2 offset = new Vector2(0, textureOffset);             //set offset of road 
+        // road.material.SetTextureOffset(MainTex, offset);            //move road by changing the texture
     }
 
     public void SetSpeed(float s)
